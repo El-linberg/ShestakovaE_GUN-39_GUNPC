4edb3f5efadd51868b659e0ed1b1499ed1264b23
Elyash <elai.linberg@gmail.com>
2025-09-23 19:40:41 +0300

    Class

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Classes
+{
+    class Unit
+    {
+        private float _health;
+        private float _armor;
+        public string Name { get; }
+        public float Health => _health;
+
+        public int Damage { get; }
+
+
+        public Unit() : this(name: "Unknown Unit")
+        {
+
+        }
+        public Unit(string name)
+        {
+            Name = name;
+        }
+        public float RealHealth()
+        {
+            return Health * (1 + Armor);
+        }
+
+        public float Armor
+        {
+            get
+            {
+
+                return (float) Math.Round(_armor, 2);
+            }
+            set
+            {
+                if (value >= 0 || value <= 1)
+                {
+                    _armor = value;
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+
+        public Unit(int damage)
+        {
+            Damage = 5;
+        }
+
+        public bool SetDamage()
+        {
+            _health = Health - Damage * Armor;
+            {
+                if (Health <= 0f) return true;
+                else return false;
+
+            }
+        }
+    }
+}
