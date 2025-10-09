8eee2b9c1cfbaef2ba0ee13100244fbe8d3fb662
Elyash <elai.linberg@gmail.com>
2025-10-09 13:27:36 +0300

    golovolomka

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -1,151 +1,157 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 
 namespace Classes
 {
-    class Unit
+    public class Program
     {
-        private float _health;
-        public string Name { get; }
-        public float Health => _health;
 
-        public int Damage { get; }
-        public float Armor { get; private set; }
-
-        public Unit() : this(name: "Unknown Unit")
+        public static void Main(string[] args)
         {
-
-        }
-        public Unit(string name)
-        {
-            Name = name;
-            Damage = 5;
-            Armor = 0.6f;
-        }
-        public float RealHealth()
-        {
-            return Health * (1 + Armor);
+            Dungeon dungeon = new();
         }
 
-
-        public bool SetDamage()
+        public class Unit
         {
-            _health = Health - Damage * Armor;
+            private float _health;
+            public string Name { get; }
+            public float Health => _health;
 
-            if (Health <= 0f)
-                return true;
-            else
-                return false;
+            public Interval Damage { get; }
+            public float Armor { get; private set; }
 
+            public Unit(string name)
+            {
+                Name = name;
+            }
+            public Unit(string name, int MinDamage, int MaxDamage)
+            {
+                Name = name;
+                Armor = 0.6f;
+                SetDamage(MinDamage, MaxDamage);
+            }
+            public float RealHealth()
+            {
+                return Health * (1 + Armor);
+            }
+
+
+            public bool SetDamage(int MinDamage, int MaxDamage)
+            {
+                _health = Health - ((MinDamage + MaxDamage) / 2) * Armor;
+
+                if (Health <= 0f)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public class Weapon
+        {
+
+            public string Name { get; }
+
+            public Interval Damage { get; }
+
+            public float Durability { get; }
+
+            public Weapon(string name)
+            {
+                Name = name;
+            }
+            public Weapon(string name, int MinDamage, int MaxDamage) : this(name)
+            {
+                Durability = 1f;
+                SetDamageParams(MinDamage, MaxDamage);
+
+            }
+            public void SetDamageParams(int MinDamage, int MaxDamage)
+
+            {
+
+
+            }
+        }
+
+        public struct Interval
+        {
+            public int Min { get; }
+            public int Max { get; }
+            private Random random;
+
+            public Interval(int minValue, int maxValue) : this()
+            {
+                if (minValue > maxValue)
+                {
+                    int t = minValue;
+                    minValue = maxValue;
+                    maxValue = t;
+                    Console.WriteLine("Некорректные входные данные");
+                }
+
+                if (minValue < 0)
+                {
+                    minValue = 0;
+
+                    Console.WriteLine("Некорректные входные данные");
+                }
+                if (maxValue < 0)
+                {
+                    maxValue = 0;
+
+                    Console.WriteLine("Некорректные входные данные");
+
+                }
+                if (minValue == maxValue)
+                {
+                    int t = maxValue + 10;
+                    maxValue = t;
+
+                    Console.WriteLine("Некорректные входные данные");
+                }
+                Min = minValue;
+                Max = maxValue;
+                random = new Random();
+            }
+        }
+        public struct Room
+        {
+            public Unit unit;
+            public Weapon weapon;
+            public Room(Unit unit, Weapon weapon)
+            {
+            }
+
+
+        }
+
+        public class Dungeon
+        {
+            public Room[] rooms { get; private set; }
+            Room[] room = new Room[3]
+
+            {   new Room (new Unit("Юнит 1"), new Weapon("Оружие 1")),
+                new Room (new Unit("Юнит 2"), new Weapon("Оружие 2")),
+                new Room (new Unit("Юнит 3"), new Weapon("Оружие 3"))
+            };
+            public void ShowRooms()
+            {
+                for (int i = 0; i < rooms.Length; i++) // поле типа Room[]
+                {
+                    var room = rooms[i]; // Берёте по индексу
+                                         //Выводите на экран вот таким образом! Подробнее об этом в уроке про строки.
+                    Console.WriteLine("unit of room" + room.unit);
+                    Console.WriteLine("weapon of room" + room.weapon);
+                    Console.WriteLine("—");
+                }
+
+            }
 
         }
     }
-    class Weapon
-    {
-
-        public string Name { get; }
-
-        public int MinDamage { get; private set; }
-
-        public int MaxDamage { get; private set; }
-        public float Durability { get; }
-
-        public Weapon(string name)
-        {
-            Name = name;
-        }
-        public Weapon(string name, int MinDamage, int MaxDamage) : this(name)
-        {
-            Durability = 1f;
-            SetDamageParams();
-
-        }
-        public void SetDamageParams()
-
-        {
-
-
-        }
-    }
-
-    public struct Interval()
-    {
-        public float Min { get; }
-        public float Max { get; }
-        public float t;
-        private Random random;
-
-        public Interval(float minValue, float maxValue)
-        {
-            random = new Random();
-            Min = minValue;
-            Max = maxValue;
-            if (minValue > maxValue)
-            {
-                t = minValue;
-                minValue = maxValue;
-                maxValue = t;
-                Console.WriteLine("Некорректные входные данные");
-            }
-
-            if (minValue < 0)
-            {
-                minValue = 0;
-
-                Console.WriteLine("Некорректные входные данные");
-            }
-            if (maxValue < 0)
-            {
-                maxValue = 0;
-
-                Console.WriteLine("Некорректные входные данные");
-
-            }
-            if (minValue == maxValue)
-            {
-                t = maxValue + 10;
-                maxValue = t;
-
-                Console.WriteLine("Некорректные входные данные");
-            }
-            Min = minValue;
-            Max = maxValue;
-        }
-    }
-    public struct Room()
-    {
-        Unit unit = new Unit();
-        Weapon weapon = new Weapon("Оружие");
-        public Room[] rooms { get; private set; }
-        Room[] room = new Room[3];
-        public void ShowRooms()
-        {
-            for (int i = 0; i < rooms.Length; i++) // поле типа Room[]
-            {
-                var room = rooms[i]; // Берёте по индексу
-                                     //Выводите на экран вот таким образом! Подробнее об этом в уроке про строки.
-                Console.WriteLine("unit of room" +room.unit);
-                Console.WriteLine("weapon of room" +room.weapon);
-                Console.WriteLine("—");
-            }
-
-        }
-    }
-
-    public class Dangeon()
-    {
-        Room[] room = new Room[3];
-         
-        new Room[(new Unit("Юнит 1"), new Weapon("Оружие 1")),
-            (new Unit("Юнит 2"), new Weapon("Оружие 2")),
-            (new Unit("Юнит 3"), new Weapon("Оружие 3"))
-            ];
-       
-
-    }
-
 
 }
 
--- Classes/Classes/obj/Debug/net8.0/Classes.AssemblyInfo.cs
+++ Classes/Classes/obj/Debug/net8.0/Classes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca580a166a860a16fb102fd79cb2ce9cdc16cc4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28582b697f4c31fa6b9ce8398523e7ac5a1c6817")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
